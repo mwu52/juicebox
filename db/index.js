d8b9de5bfcad2c0d4faac9be19cf062b603961a6
Michael Wu <mwu52@uic.edu>
2022-07-15 14:08:43 -0500

    update

--- db/index.js
+++ db/index.js
@@ -2,6 +2,21 @@ const { Client } = require("pg");
 
 const client = new Client("postgres://localhost:5432/juicebox-dev");
 
+async function createUser({ username, password }) {
+    try {
+      const { rows } = await client.query(`
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
 async function getAllUsers() {
   const { rows } = await client.query(
     `SELECT id, username 
@@ -12,20 +27,7 @@ async function getAllUsers() {
   return rows;
 }
 
-async function createUser({ username, password }) {
-  try {
-    const { rows } = await client.query(`
-    INSERT INTO users(username, password) 
-    VALUES($1, $2) 
-    ON CONFLICT (username) DO NOTHING 
-    RETURNING *;
-  `, [username, password]);
 
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
 
 module.exports = {
   client,
