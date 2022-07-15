7a6e21ab9002c37d235c81b86ca2fd154af1eb24
John-Anthony <jharwerth@udallas.edu>
2022-07-15 13:14:33 -0500

    finished Part 1.1

--- db/index.js
+++ db/index.js
@@ -1,32 +1,34 @@
-const { Client } = require('pg');
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const { Client } = require("pg");
 
+const client = new Client("postgres://localhost:5432/juicebox-dev");
 
 async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username 
+  const { rows } = await client.query(
+    `SELECT id, username 
       FROM users;
-    `);
-  
+    `
+  );
+
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(`
+    INSERT INTO users(username, password) 
+    VALUES($1, $2) 
+    ON CONFLICT (username) DO NOTHING 
+    RETURNING *;
+  `, [username, password]);
+
     return rows;
+  } catch (error) {
+    throw error;
   }
+}
 
-  async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-      INSERT INTO users(username, password) VALUES ($1, $2);
-      `, [ "some_name", "some_password" ]);
-  
-      return result
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  // and export them
-  module.exports = {
-    client,
-    getAllUsers,
-    createUser,
-  }
-
+module.exports = {
+  client,
+  getAllUsers,
+  createUser,
+};
