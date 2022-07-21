c3497ccf28ae077bc9e04b949fa7756507963368
Michael Wu <mwu52@uic.edu>
2022-07-21 10:31:52 -0500

    heroku setup again 2

--- db/index.js
+++ db/index.js
@@ -1,9 +1,8 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
+const client = new Client(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  );
 
 async function createUser({ username, password, name, location }) {
   try {
