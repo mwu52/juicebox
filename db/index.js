31d57fb5492204c1b34749e4711fbe00c6524623
Michael Wu <mwu52@uic.edu>
2022-07-21 10:20:32 -0500

    heroku setup

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 
-const client = new Client("postgres://localhost:5432/juicebox-dev");
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 async function createUser({ username, password, name, location }) {
   try {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PORT = 3000;
+const  { PORT = 3000 } = process.env
 const express = require('express');
 const server = express();
 const morgan = require('morgan');
